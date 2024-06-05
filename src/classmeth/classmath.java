c3d787ad12769f0a5588cb810887d081708850c1
Aluno <Aluno@10.85.198.36>
2024-06-05 17:07:24 -0300

    guit

---
+++ src/classmeth/classmath.java
@@ -0,0 +1,17 @@
+package math;
+
+
+public class classmath{
+    
+     public static void main(String[] args) {
+         
+        int b = 3;
+        int h = 4;
+        
+        
+        double z = b*h*0.5;
+        System.out.println(z);
+          
+         }
+        
+     }
